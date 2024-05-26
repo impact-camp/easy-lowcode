e66292377e5ccbaa945f85575e82cb270e0f5360
woai3c <411020382@qq.com>
2024-05-26 23:25:01 +0800

    chore: change eslint rules.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,9 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
-    browser: true
+    es6: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  extends: ['eslint-config-airbnb-vue3-ts'],
 }
