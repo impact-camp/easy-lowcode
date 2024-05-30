129f0f5345a873ac316720f378920df5095465fd
woai3c <411020382@qq.com>
2024-05-31 01:21:04 +0800

    build: add sourcemap and minify config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import type { ConfigEnv, UserConfig } from 'vite'
+import { loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
+const root: string = process.cwd()
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_PORT, VITE_PUBLIC_PATH } = loadEnv(mode, root)
+
+  const config: UserConfig = {
+    base: VITE_PUBLIC_PATH,
+    plugins: [vue(), vueJsx(), VueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      port: Number(VITE_PORT),
+      host: '0.0.0.0',
+      proxy: {},
+    },
+    build: {
+      minify: 'terser',
+    },
   }
-})
+
+  if (mode === 'production') {
+    config.build!.terserOptions = {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    }
+  } else {
+    config.build!.sourcemap = true
+  }
+
+  return config
+}
